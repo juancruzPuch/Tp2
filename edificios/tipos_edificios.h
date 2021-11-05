dee4bba9677702b442c38a6ee20f30c4a1a22aa5
Camila <cstorni@fi.uba.ar>
2021-11-05 17:51:16 -0300

    indentacion

--- edificios/tipos_edificios.h
+++ edificios/tipos_edificios.h
@@ -5,22 +5,22 @@ const std::string PATH_ENTRADA_EDIFICIOS = "edificios.txt";
 
 class Tipo_edificio {
 
-protected:
-	std::string tipo_edificio;
-	int cant_piedra;
-	int cant_madera;
-	int cant_metal;
-	int cant_max_construido;
+	protected:
+		std::string tipo_edificio;
+		int cant_piedra;
+		int cant_madera;
+		int cant_metal;
+		int cant_max_construido;
 
-public:
+	public:
 
-	Tipo_edificio (int piedra, int madera, int metal, int cant_max_construido, std::string tipo_edificio);
-	int obtener_piedra();
-	int obtener_madera();
-	int obtener_metal();
-	int obtener_cant_max_construido();
-	std::string obtener_tipo();
-	void mostrar();
+		Tipo_edificio (int piedra, int madera, int metal, int cant_max_construido, std::string tipo_edificio);
+		int obtener_piedra();
+		int obtener_madera();
+		int obtener_metal();
+		int obtener_cant_max_construido();
+		std::string obtener_tipo();
+		void mostrar();
 };
 
 
---
+++ edificios/tipos_edificios.h~
@@ -0,0 +1,27 @@
+#ifndef TIPOS_EDIFICIOS_H
+#define TIPOS_EDIFICIOS_H
+
+const std::string PATH_ENTRADA_EDIFICIOS = "edificios.txt";
+
+class Tipo_edificio {
+
+protected:
+	std::string tipo_edificio;
+	int cant_piedra;
+	int cant_madera;
+	int cant_metal;
+	int cant_max_construido;
+
+public:
+
+	Tipo_edificio (int piedra, int madera, int metal, int cant_max_construido, std::string tipo_edificio);
+	int obtener_piedra();
+	int obtener_madera();
+	int obtener_metal();
+	int obtener_cant_max_construido();
+	std::string obtener_tipo();
+	void mostrar();
+};
+
+
+#endif //TIPOS_EDIFICIOS_H
